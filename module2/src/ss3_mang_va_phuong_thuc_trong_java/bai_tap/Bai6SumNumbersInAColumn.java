d526dce10426c5160a98850f08c0c22a09958088
lethanhhau <haule19952017@gmail.com>
2022-04-05 08:13:43 +0700

    nop bai

---
+++ module2/src/ss3_mang_va_phuong_thuc_trong_java/bai_tap/Bai6SumNumbersInAColumn.java
@@ -0,0 +1,22 @@
+package ss3_mang_va_phuong_thuc_trong_java.bai_tap;
+//Tính tổng các số ở một cột xác định.
+
+import java.util.Scanner;
+
+public class Bai6SumNumbersInAColumn {
+    public static void main(String[] args) {
+        int[][]arr ={
+                {2,4,5,6},
+                {4,8,9,2},
+                {6,9,2}
+        };
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Nhập số cột muốn tính tổng:");
+        int j = sc.nextInt();
+        int sum =0;
+        for (int i = 0; i < arr[j].length; i++) {
+            sum += arr[j][i];
+        }
+        System.out.println("tổng của cột "+ j +" = "+ sum);
+    }
+}
