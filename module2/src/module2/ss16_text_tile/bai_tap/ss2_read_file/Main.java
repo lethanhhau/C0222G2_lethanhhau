fe468e80aa3d549df8bda8db095475385454b057
lethanhhau <haule19952017@gmail.com>
2022-04-25 10:32:03 +0700

    nop bai

---
+++ module2/src/module2/ss16_text_tile/bai_tap/ss2_read_file/Main.java
@@ -0,0 +1,54 @@
+package module2.ss16_text_tile.bai_tap.ss2_read_file;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    private final static String COUNTRIES ="src/module2/ss16_text_tile/bai_tap/ss2_read_file/countries.csv";
+    public static void main(String[] args) {
+
+        BufferedReader br = null;
+        try {
+            String line;
+            br = new BufferedReader(new FileReader(COUNTRIES));
+
+            while ((line = br.readLine()) != null) {
+                printCountry(parseCsvLine(line));
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (br != null)
+                    br.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static List<String> parseCsvLine(String csvLine) {
+        List<String> result = new ArrayList<>();
+        if (csvLine != null) {
+            String[] splitData = csvLine.split(",");
+            for (int i = 0; i < splitData.length; i++) {
+                result.add(splitData[i]);
+            }
+        }
+        return result;
+    }
+
+    private static void printCountry(List<String> country) {
+        System.out.println(
+                "Country [id= "
+                        + country.get(0)
+                        + ", code= " + country.get(1)
+                        + " , name=" + country.get(2)
+                        + "]");
+    }
+
+}
