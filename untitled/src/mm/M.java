fbe62a06d823d909e1aa644576896bd6348777b7
lethanhhau <haule19952017@gmail.com>
2022-05-26 11:35:30 +0700

    nop bai

---
+++ untitled/src/mm/M.java
@@ -0,0 +1,16 @@
+package mm;
+
+public class M {
+    public static void main(String[] args) {
+        int[][] arr = {
+                {1,2,8,43},
+                {9,4,53,7},
+                {11,6,4,0},
+                {1,2,6,51}
+        };
+    }
+    public boolean check(){
+
+        return check();
+    }
+}
