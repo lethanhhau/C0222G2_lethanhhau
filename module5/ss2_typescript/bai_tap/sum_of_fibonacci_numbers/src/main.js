fc35b35f88ffc7ca2a0ada32209cad5b0dede743
lethanhhau <haule19952017@gmail.com>
2022-07-18 17:08:34 +0700

    nop bai

---
+++ module5/ss2_typescript/bai_tap/sum_of_fibonacci_numbers/src/main.js
@@ -0,0 +1,16 @@
+function fibonacci(index) {
+    if (index == 0) {
+        return index;
+    }
+    if (index == 1) {
+        return index;
+    }
+    return fibonacci(index - 1) + fibonacci(index - 2);
+}
+var sum = 0;
+var n = 20;
+for (var i = 0; i < n; i++) {
+    sum += fibonacci(i);
+    console.log(fibonacci(i));
+}
+console.log(sum);
