9875b85c866f884c01d7e55e3d22e5aeae76825b
lethanhhau <haule19952017@gmail.com>
2022-09-05 16:03:16 +0700

    update

--- sprint2/font-end/shop-online/src/app/app-routing.module.ts
+++ sprint2/font-end/shop-online/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {HomeComponent} from './common/home/home.component';
 import {CartComponent} from './cart/cart.component';
+import {HomeLoginComponent} from './login/home-login/home-login.component';
 
 
 const routes: Routes = [
   { path: '', component: HomeComponent},
-  { path: 'cart', component: CartComponent}
+  { path: 'cart', component: CartComponent},
+  { path: 'login', component: HomeLoginComponent}
 ];
 
 @NgModule({
--- sprint2/font-end/shop-online/src/app/login/home-login/home-login.component.ts
+++ sprint2/font-end/shop-online/src/app/login/home-login/home-login.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-home-login',
@@ -7,9 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeLoginComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
 
+
+  sing() {
+    const signUpButton = document.getElementById('signUp');
+    const signInButton = document.getElementById('signIn');
+    const container = document.getElementById('container');
+
+    signUpButton.addEventListener('click', () => {
+      container.classList.add('right-panel-active');
+    });
+
+    signInButton.addEventListener('click', () => {
+      container.classList.remove('right-panel-active');
+    });
+  }
 }
